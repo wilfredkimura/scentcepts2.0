6d51e1eb1ef04687ecf03fafe00b970fccf5ad8b
wilfredkimura <wilfredmutahi9@gmail.com>
2026-06-26 16:59:50 +0300

    feat: initialize application class (ScentceptsApplication main class) with async support and update Spring Boot web dependencies (build.gradle)

--- src/main/java/ke/co/scentcepts/ScentceptsApplication.java
+++ src/main/java/ke/co/scentcepts/ScentceptsApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@EnableAsync // Enables asynchronous processing for event listeners
 @SpringBootApplication
+@EnableAsync // Enables asynchronous processing for event listeners (@EventListener)
 public class ScentceptsApplication {
 
     public static void main(String[] args) {
