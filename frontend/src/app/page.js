b713d3d3c2ec41d1ce2ac21a83e89bc33a71e5a8
wilfredkimura <wilfredmutahi9@gmail.com>
2026-06-28 17:48:11 +0300

    fix: Changed hero image

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -30,7 +30,7 @@ export default function LandingPage() {
       <section className="relative min-h-[85vh] flex items-center justify-center overflow-hidden">
         <div className="absolute inset-0 z-0">
           <img
-            src="https://images.unsplash.com/photo-1615397323282-31251f77569e?auto=format&fit=crop&q=80&w=2000"
+            src="https://images.unsplash.com/photo-1595425959632-34f2822322ce?q=80&w=1098&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt="Luxury Perfume"
             className="w-full h-full object-cover opacity-40 img-awaken"
           />
